32fdd5b294b41b3534f39f6b2392061d5831c948
Vitaliy Oganesyan <oganesyanvitaly@gmail.com>
2022-05-19 23:41:52 +0300

    added image property

--- custom_paintable/src/custom_paintable/imp.rs
+++ custom_paintable/src/custom_paintable/imp.rs
@@ -1,12 +1,13 @@
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{gdk, glib, graphene, cairo};
+use once_cell::sync::Lazy;
+use glib::{BindingFlags, ParamFlags, ParamSpec, ParamSpecObject, Value};
 
-use opencv::{prelude::*};
 
 #[derive(Default)]
 pub struct CustomPaintable {
-    pub image: Mat,
+    pub image: RefCell<Option<cairo::ImageSurface>
 }
 
 // The central trait for subclassing a GObject
@@ -18,7 +19,57 @@ impl ObjectSubclass for CustomPaintable {
 }
 
 // Trait shared by all GObjects
-impl ObjectImpl for CustomPaintable {}
+impl ObjectImpl for CustomPaintable {
+    fn properties() -> &'static [ParamSpec] {
+        static PROPERTIES: Lazy<Vec<ParamSpec>> = Lazy::new(|| {
+            vec![ParamSpecObject::new(
+                // Name
+                "image",
+                // Nickname
+                "image",
+                // Short description
+                "image",
+                // Object type
+                cairo::ImageSurface::static_type(),
+                // The property can be read and written to
+                ParamFlags::READWRITE,
+            )]
+        });
+        PROPERTIES.as_ref()
+    }
+
+    fn set_property(
+        &self,
+        obj: &Self::Type,
+        _id: usize,
+        value: &glib::Value,
+        pspec: &glib::ParamSpec,
+    ) {
+        match pspec.name() {
+            "image" => {
+                self.set_image(obj, value.get::<cairo::ImageSurface>().ok().as_ref());
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    fn property(&self, obj: &Self::Type, _id: usize, pspec: &glib::ParamSpec) -> glib::Value {
+        match pspec.name() {
+            "image" => self.image(obj).to_value(),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn constructed(&self, obj: &Self::Type) {
+        self.parent_constructed(obj);
+
+        // Bind label to number
+        // `SYNC_CREATE` ensures that the label will be immediately set
+        // obj.bind_property("number", obj, "label")
+        //     .flags(BindingFlags::SYNC_CREATE)
+        //     .build();
+    }
+}
 
 // Trait shared by all paintables
 impl PaintableImpl for CustomPaintable {
@@ -41,39 +92,6 @@ impl PaintableImpl for CustomPaintable {
         // Draw image
         let ctx = snapshot.append_cairo(&graphene::Rect::new(0_f32, 0_f32, width as f32, height as f32));
         ctx.set_source_rgb(1.0, 0.0, 0.0);
-
-        let rows = self.image.rows();
-        let cols = self.image.cols();
-        let stride = self.image.mat_step()[0];
-
-        let n_bytes = rows as usize * stride;
-        let data = unsafe { std::slice::from_raw_parts(self.image.data(), n_bytes) };
-        let data = data.to_vec();
-        let image_surface = cairo::ImageSurface::create_for_data(
-            data,
-            cairo::Format::Rgb24,
-            cols,
-            rows,
-            stride as i32
-        );
         ctx.paint().unwrap();
-        // // Draw a linear gradient
-        // snapshot.append_linear_gradient(
-        //     &graphene::Rect::new(0_f32, 0_f32, width as f32, height as f32),
-        //     &graphene::Point::new(0f32, 0f32),
-        //     &graphene::Point::new(width as f32, height as f32),
-        //     &[
-        //         gsk::ColorStop::new(0.0, gdk::RGBA::RED),
-        //         gsk::ColorStop::new(0.15, gdk::RGBA::new(1.0, 127_f32 / 255_f32, 0.0, 1.0)),
-        //         gsk::ColorStop::new(0.3, gdk::RGBA::new(1.0, 1.0, 0.0, 1.0)),
-        //         gsk::ColorStop::new(0.45, gdk::RGBA::GREEN),
-        //         gsk::ColorStop::new(0.6, gdk::RGBA::BLUE),
-        //         gsk::ColorStop::new(
-        //             0.75,
-        //             gdk::RGBA::new(75_f32 / 255_f32, 0.0, 130_f32 / 255_f32, 1.0),
-        //         ),
-        //         gsk::ColorStop::new(0.9, gdk::RGBA::new(143_f32 / 255_f32, 0.0, 1.0, 1.0)),
-        //     ],
-        // );
     }
 }
--- custom_paintable/src/custom_paintable/mod.rs
+++ custom_paintable/src/custom_paintable/mod.rs
@@ -1,7 +1,6 @@
 mod imp;
 
 use gtk::{gdk, glib};
-use opencv::{core::Mat};
 
 glib::wrapper! {
     pub struct CustomPaintable(ObjectSubclass<imp::CustomPaintable>) @implements gdk::Paintable;
@@ -15,11 +14,6 @@ impl Default for CustomPaintable {
 
 impl CustomPaintable {
     pub fn new() -> Self {
-        glib::Object::new(&[("image", Mat::default())]).expect("Failed to create a CustomPaintable")
-    }
-
-    pub fn set_image(&self, image: Mat) {
-        println!("set_image");
-        self.imp().image = image;
+        glib::Object::new(&[]).expect("Failed to create a CustomPaintable")
     }
 }
