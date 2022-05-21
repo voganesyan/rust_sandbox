3433bf9e86075dc0d4233a7ef34ec6a57736641e
Vitaliy Oganesyan <oganesyanvitaly@gmail.com>
2022-05-21 11:57:47 +0300

    code clean up

--- face_detector/src/face_detector/face_detector.rs
+++ face_detector/src/face_detector/face_detector.rs
@@ -1,26 +1,17 @@
 use std::error::Error;
 use std::path::PathBuf;
 use std::result::Result;
-use tensorflow::Code;
-use tensorflow::Graph;
-use tensorflow::SavedModelBundle;
-use tensorflow::SessionOptions;
-use tensorflow::SessionRunArgs;
-use tensorflow::Status; 
-use tensorflow::Tensor;
-use tensorflow::DEFAULT_SERVING_SIGNATURE_DEF_KEY;
-
-use tensorflow::eager;
+use tensorflow as tf;
 
 use opencv::{prelude::*, core::*, imgproc};
 
-fn cv_mat_to_tf_tensor(image: &Mat) -> Tensor::<f32> {
+fn cv_mat_to_tf_tensor(image: &Mat) -> tf::Tensor::<f32> {
     let rows = image.rows();
     let cols = image.cols();
     let mut normalized = Mat::default();
     image.convert_to(&mut normalized, CV_32FC3, 1.0, 0.0).unwrap(); 
     // where normalized is an opencv matrix already converted to floating point and normalized to 0..1.
-    let mut input_tensor = Tensor::<f32>::new(&[1, rows as u64, cols as u64, 3]);
+    let mut input_tensor = tf::Tensor::<f32>::new(&[1, rows as u64, cols as u64, 3]);
     let ptr = input_tensor.as_mut_ptr() as *mut std::ffi::c_void;
     let mut input_mat = unsafe {
         Mat::new_rows_cols_with_data(
@@ -42,9 +33,9 @@ fn cv_mat_to_tf_tensor(image: &Mat) -> Tensor::<f32> {
 
 
 pub struct Detector {
-    op_x: tensorflow::Operation,
-    op_output: tensorflow::Operation,
-    bundle: tensorflow::SavedModelBundle
+    op_x: tf::Operation,
+    op_output: tf::Operation,
+    bundle: tf::SavedModelBundle
 }
 
 impl Detector {
@@ -53,8 +44,8 @@ impl Detector {
         let model_file: PathBuf = [export_dir, "saved_model.pb"].iter().collect();
         if !model_file.exists() {
             return Err(Box::new(
-                Status::new_set(
-                    Code::NotFound,
+                tf::Status::new_set(
+                    tf::Code::NotFound,
                     &format!(
                         "Run 'python src/mobilenetv3/create_model.py' to generate \
                          {} and try again.",
@@ -66,19 +57,19 @@ impl Detector {
         }
     
         // Create an eager execution context
-        let opts = eager::ContextOptions::new();
-        let ctx = eager::Context::new(opts)?;
+        let opts = tf::eager::ContextOptions::new();
+        let _ctx = tf::eager::Context::new(opts)?;
     
         // Load the model.
-        let mut graph = Graph::new();
+        let mut graph = tf::Graph::new();
 
         let bundle =
-        SavedModelBundle::load(&SessionOptions::new(), &["serve"], &mut graph, export_dir)?;
+        tf::SavedModelBundle::load(&tf::SessionOptions::new(), &["serve"], &mut graph, export_dir)?;
 
         // get in/out operations
         let signature = bundle
             .meta_graph_def()
-            .get_signature(DEFAULT_SERVING_SIGNATURE_DEF_KEY)?;
+            .get_signature(tf::DEFAULT_SERVING_SIGNATURE_DEF_KEY)?;
         let x_info = signature.get_input("input_1")?;
         let op_x = graph.operation_by_name_required(&x_info.name().name)?;
         let output_info = signature.get_output("Predictions")?;
@@ -100,14 +91,14 @@ impl Detector {
         let x = cv_mat_to_tf_tensor(&small_image);
     
         // Run the graph.
-        let mut args = SessionRunArgs::new();
+        let mut args = tf::SessionRunArgs::new();
         args.add_feed(&self.op_x, 0, &x);
         let token_output = args.request_fetch(&self.op_output, 0);
         let session = &self.bundle.session;
         session.run(&mut args).unwrap();
     
         // Check the output.
-        let output: Tensor<f32> = args.fetch(token_output)?;
+        let output: tf::Tensor<f32> = args.fetch(token_output)?;
     
         // Calculate argmax of the output
         let (max_idx, _max_val) =
